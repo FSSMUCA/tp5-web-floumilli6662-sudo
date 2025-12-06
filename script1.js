b8d75d0476efe2ffdcb86bf4e8227d39fd0f021a
fatima zahra <f.loumilli6662@uca.ac.ma>
2025-12-06 12:49:15 +0100

    depot TP5

---
+++ script1.js
@@ -0,0 +1,12 @@
+let x = "150";
+let y = 150;
+let z = true;
+
+console.log("Type de x :", typeof x); 
+console.log("Type de y :", typeof y); 
+console.log("Type de z :", typeof z); 
+
+x = Number(x);
+
+console.log("Nouvelle valeur de x :", x);
+console.log("Nouveau type de x :", typeof x); 
---
+++ script2.js
@@ -0,0 +1,13 @@
+let prenom = "Sara";
+let age = 22;
+
+let phrase = "Je m'appelle " + prenom + " et j'ai " + age + " ans.";
+
+console.log(phrase);
+
+let nouvelAge = 23;     
+age = nouvelAge;       
+
+phrase = "Je m'appelle " + prenom + " et j'ai " + age + " ans.";
+
+console.log(phrase);
